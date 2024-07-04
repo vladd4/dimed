f2f190b169cecc225917c81f6ecfc2842c4d8542
Vladyslav D <donetsvlad444@gmail.com>
2024-07-04 12:56:47 +0300

    updated service label

--- src/components/ServiceApoint/ServiceApoint.tsx
+++ src/components/ServiceApoint/ServiceApoint.tsx
@@ -12,9 +12,7 @@ export default function ServiceApoint({ id }: AppointProps) {
   const dispatch = useAppDispatch();
   return (
     <section className={styles.root}>
-      <p className={styles.label}>
-        Запишіться на лікувальний масаж в DIMED вже сьогодні!
-      </p>
+      <p className={styles.label}>Запишіться в DIMED вже сьогодні!</p>
       <p>
         Звертаючись до нашого центру DIMED у Вінниці, ви можете розраховувати на
         індивідуальний підхід, який враховує ваші потреби та проблеми зі
