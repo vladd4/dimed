e8aa724956160da740186b572fa3a3b9cf7f3bc0
Vladyslav D <donetsvlad444@gmail.com>
2024-07-11 11:35:32 +0300

    updated layout and sitemap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,15 +11,16 @@ export const metadata: Metadata = {
   title: "DIMED - центр вертебрології та реабілітації у Вінниці",
   description:
     "Наша команда надає ряд послуг, а саме консультації невролога, вертебролога, сучасні високоінтенсивні фізіотерапевтичні процедури, якісний лікувальний масаж, комплексну індивідуально підібрану лікувальну фізкультуру.",
-  // assets: ["https://dimed.vercel.app/"],
+  assets: ["https://dimed.vn.ua/"],
   openGraph: {
     title: "DIMED - центр вертебрології та реабілітації у Вінниці",
     description:
       "Наша команда надає ряд послуг, а саме консультації невролога, вертебролога, сучасні високоінтенсивні фізіотерапевтичні процедури, якісний лікувальний масаж, комплексну індивідуально підібрану лікувальну фізкультуру.",
     type: "website",
     siteName: "Dimed",
-    // url: "https://dimed.vercel.app/",
+    url: "https://dimed.vn.ua/",
   },
+  metadataBase: new URL("https://dimed.vn.ua/"),
 };
 
 export default function RootLayout({
@@ -35,7 +36,16 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1, user-scalable=no"
         />
-        <link rel="canonical" href="" />
+        <meta
+          name="google-site-verification"
+          content="3Bx8YPxhp1yGn-NYElVeQyzLptKEu1PmTChP9V267hk"
+        />
+        <link rel="canonical" href="https://dimed.vn.ua/" />
+        <link
+          rel="icon"
+          href="https://dimed.vn.ua/favicon.ico"
+          type="image/x-icon"
+        />
       </head>
       <body>
         <ReduxProvider>
