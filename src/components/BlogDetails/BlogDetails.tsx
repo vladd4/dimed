e540d1d15e4dd11b04a7f8c089b5e0e08a8d0893
Vladyslav D <donetsvlad444@gmail.com>
2024-07-16 19:21:20 +0300

    adding retry fetch logic for blogs

--- src/components/BlogDetails/BlogDetails.tsx
+++ src/components/BlogDetails/BlogDetails.tsx
@@ -34,32 +34,48 @@ type BlogProps = {
 export default function BlogDetails({ id }: BlogProps) {
   const [blog, setBlog] = useState<BlogItem>();
 
-  const fetchBlogById = async (id: string) => {
-    const q = query(collection(db, "blog"), where("heading_1", "==", id));
+  const fetchBlogById = async (id: string, retryAttempts = 3) => {
+    let attempt = 0;
+    while (attempt < retryAttempts) {
+      try {
+        const q = query(collection(db, "blog"), where("heading_1", "==", id));
+        const querySnapshot = await getDocs(q);
 
-    const querySnapshot = await getDocs(q);
+        if (!querySnapshot.empty) {
+          const docSnap = querySnapshot.docs[0];
+          const data = docSnap.data();
 
-    if (!querySnapshot.empty) {
-      const docSnap = querySnapshot.docs[0];
-      const data = docSnap.data();
-
-      const blogItem = {
-        heading_1: data.heading_1,
-        heading_2: data.heading_2,
-        heading_3: data.heading_3,
-        heading_4: data.heading_4,
-        heading_5: data.heading_5,
-        paragraph_1: data.paragraph_1,
-        paragraph_2: data.paragraph_2,
-        paragraph_3: data.paragraph_3,
-        paragraph_4: data.paragraph_4,
-        paragraph_5: data.paragraph_5,
-        image_1: data.image_1,
-        image_2: data.image_2,
-        author: data.author,
-        date: data.date,
-      };
-      setBlog(blogItem);
+          const blogItem = {
+            heading_1: data.heading_1,
+            heading_2: data.heading_2,
+            heading_3: data.heading_3,
+            heading_4: data.heading_4,
+            heading_5: data.heading_5,
+            paragraph_1: data.paragraph_1,
+            paragraph_2: data.paragraph_2,
+            paragraph_3: data.paragraph_3,
+            paragraph_4: data.paragraph_4,
+            paragraph_5: data.paragraph_5,
+            image_1: data.image_1,
+            image_2: data.image_2,
+            author: data.author,
+            date: data.date,
+          };
+          setBlog(blogItem);
+          return;
+        } else {
+          console.error(`Document with heading_1 "${id}" does not exist.`);
+        }
+      } catch (error) {
+        console.error("Error fetching blog:", error);
+        attempt++;
+        if (attempt < retryAttempts) {
+          console.log(`Retrying fetch, attempt ${attempt}...`);
+          await new Promise((resolve) => setTimeout(resolve, 500));
+        } else {
+          console.error("Maximum retry attempts exceeded.");
+        }
+      }
     }
   };
 
