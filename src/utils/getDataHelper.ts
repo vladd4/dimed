2b5fc6db6c7e938a1f9b52148bdfd929cd0117c1
Vladyslav D <donetsvlad444@gmail.com>
2024-07-17 18:18:23 +0300

    updated base api endpoint

--- src/utils/getDataHelper.ts
+++ src/utils/getDataHelper.ts
@@ -1,6 +1,6 @@
 export async function getData(endpoint: string) {
   try {
-    const res = await fetch(`http://localhost:3000/api${endpoint}`);
+    const res = await fetch(`https://dimed.vn.ua/api${endpoint}`);
 
     if (!res.ok) {
       throw new Error(`Failed to fetch data: ${res.status} ${res.statusText}`);
