bc8afebeb8dd251542964be95a8f5ac43a186ffe
Vladyslav D <donetsvlad444@gmail.com>
2024-07-17 18:06:42 +0300

    updated api endpoint

--- src/utils/getDataHelper.ts
+++ src/utils/getDataHelper.ts
@@ -1,8 +1,6 @@
 export async function getData(endpoint: string) {
   try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_API}${endpoint}`
-    );
+    const res = await fetch(`http://localhost:3000/api${endpoint}`);
 
     if (!res.ok) {
       throw new Error(`Failed to fetch data: ${res.status} ${res.statusText}`);
