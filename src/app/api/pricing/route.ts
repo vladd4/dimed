acaa35077e5cf2e9468f843db09e74560c2b40d2
Vladyslav D <donetsvlad444@gmail.com>
2024-07-17 17:56:23 +0300

    fixing prerender error v2

--- src/app/api/pricing/route.ts
+++ src/app/api/pricing/route.ts
@@ -1,6 +1,21 @@
 import { ServiceItem } from "@/app/types/general.types";
-import { db } from "@/firebase";
 import { collection, getDocs } from "firebase/firestore";
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
 
 export async function GET() {
   const collectionRef = collection(db, "services");
